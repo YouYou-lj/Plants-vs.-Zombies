e240dd02a228cba81dc354b0fd3c3dac783d7e86
优优 <2176325180@qq.com>
2024-11-28 12:23:08 +0800

    first Animation.cpp

---
+++ Animation.cpp
@@ -0,0 +1,15 @@
+#include "Animation.h"
+
+Animation* Animation::create() {
+    return new Animation;
+}
+
+void Animation::addImage(const char *filename) {
+    IMAGE img;
+    loadimage(&img, filename);
+    m_ImageArray.push_back(img);
+}
+
+IMAGE* Animation::index(int i) {
+    return &m_ImageArray[i];
+}
