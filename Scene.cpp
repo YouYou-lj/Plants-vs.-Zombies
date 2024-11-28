4cfc04fc56d141e4f3ca2d7667eee7a15d9bbdfd
优优 <2176325180@qq.com>
2024-11-28 12:20:51 +0800

    third Scene.cpp

--- Scene.cpp
+++ Scene.cpp
@@ -1,4 +1,5 @@
 #include "Scene.h"
+#include <cstdio>
 #include "Vec2.h"
 
 Scene *Scene::create() {
@@ -15,10 +16,21 @@ Scene *Scene::create() {
 bool Scene::init() {
     loadimage(&m_BackgroundImg, "D:/CLion 2024.2.2/Plants vs. Zombies/assets/DaytimeScene.png");
 
+    Animation* PlantAnimation = Animation::create();
+    PlantAnimation->setInterval(0.09f);
+
+    for (int i = 0; i <= 12; i++) {
+        char buff[128] = {0};
+        sprintf(buff, "D:/CLion 2024.2.2/Plants vs. Zombies/assets/PlantAnimation/Peashooter/Idle/Peashooter_Idle_%d.png", i);
+        PlantAnimation->addImage(buff);
+    }
+
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 9; j++) {
             m_PlantTable[i][j] = Plant::create("D:/CLion 2024.2.2/Plants vs. Zombies/assets/PlantAnimation/Peashooter/Idle/Peashooter_Idle_0.png");
             m_PlantTable[i][j]->setPosition(Vec2(320 + j * 96, 120 + 117 * i));
+            m_PlantTable[i][j]->setAnimation(PlantAnimation);
+            m_PlantTable[i][j]->startAnimation(true);
         }
     }
 
