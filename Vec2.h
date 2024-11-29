076f20e384fad653e0e920fc806d54653e0eadf5
优优 <2176325180@qq.com>
2024-11-29 20:55:18 +0800

    second Vec2.h

--- Vec2.h
+++ Vec2.h
@@ -2,7 +2,7 @@
 
 class Vec2 {
 public:
-    Vec2() : x(0.0f), y(0.0f) {}
-    Vec2(float x, float y) : x(x), y(y) {}
+    Vec2() :x(0.0f), y(0.0f) {}
+    Vec2(float x, float y) :x(x), y(y) {}
     float x, y;
 };
