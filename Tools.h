88ec48a440c4f5ee248dae5da38efc334b8c559f
优优 <2176325180@qq.com>
2024-11-29 20:54:47 +0800

    second Tools.h

--- Tools.h
+++ Tools.h
@@ -4,17 +4,13 @@
 #include <easyx.h>
 #pragma comment(lib,"MSIMG32.LIB")
 
-class Tools
-{
+class Tools {
 public:
-	static void putimage_alpha(int x, int y, IMAGE* img)
-	{
+	static void putimage_alpha(int x, int y, IMAGE* img) {
 		int w = img->getwidth();
 		int h = img->getheight();
 		AlphaBlend(GetImageHDC(NULL), x, y, w, h, GetImageHDC(img), 0, 0, w, h, { AC_SRC_OVER, 0, 255, AC_SRC_ALPHA });
 	}
 };
-
-
 
 #endif // !__TOOLS_H__
