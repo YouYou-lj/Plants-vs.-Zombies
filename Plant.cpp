14c428d55e3fe7f236e9fab6a4a8e402064aa207
优优 <2176325180@qq.com>
2024-11-29 20:52:33 +0800

    fourth Plant.cpp

--- Plant.cpp
+++ Plant.cpp
@@ -1,8 +1,7 @@
-#include "Plant.h"
-#include "Vec2.h"
 #include "Tools.h"
+#include "Plant.h"
 
-Plant *Plant::create() {
+Plant* Plant::create() {
     Plant* plant = new Plant;
 
     if (plant->init()) {
@@ -13,7 +12,8 @@ Plant *Plant::create() {
     return nullptr;
 }
 
-Plant *Plant::create(const char *filename) {
+Plant* Plant::create(const char* filename)
+{
     Plant* plant = new Plant;
 
     if (plant->init() && plant->setImage(filename)) {
@@ -24,35 +24,39 @@ Plant *Plant::create(const char *filename) {
     return nullptr;
 }
 
-
 bool Plant::init() {
     return true;
 }
 
+
 void Plant::drawTick() {
     if (m_IsStartAnim) {
-        Tools::putimage_alpha(m_Positon.x, m_Positon.y, m_Animation->index(m_AnimIndex));
+        Tools::putimage_alpha(m_Position.x, m_Position.y, m_Animation->index(m_AnimIndex));
     } else {
-        Tools::putimage_alpha(m_Positon.x, m_Positon.y, &m_Image);
+        Tools::putimage_alpha(m_Position.x, m_Position.y, &m_Image);
     }
+
+    Rect rect = getBoundingBox();
+    setlinecolor(RED);
+    setlinestyle(PS_SOLID, 3);
+    rectangle(rect.x, rect.y, rect.x + rect.w, rect.y + rect.h);
 }
 
 void Plant::eventTick(float delta) {
     m_AnimCount += delta;
-    m_Animation->getInterval();
 
-    if (m_AnimCount >= 0.1f) {
+    if (m_AnimCount >= m_Animation->getInterval()) {
         m_AnimIndex++;
 
         if (m_AnimIndex >= m_Animation->count()) {
             m_AnimIndex = 0;
         }
 
-        m_AnimCount -= 0.1f;
+        m_AnimCount -= m_Animation->getInterval();
     }
 }
 
-bool Plant::setImage(const char *filename) {
+bool Plant::setImage(const char* filename) {
     loadimage(&m_Image, filename);
     return true;
 }
